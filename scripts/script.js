3a36e874f1a648f59396d3e2a4d0308d8ac051ea
ionutbelean <ionutbelean4@gmail.com>
2022-04-06 17:58:20 +0300

    first-commit

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+//Exercitiul 1
+const exercise1 = 2 + 3 * 4 - 5;
+console.log(exercise1);
+
+//Exercitiul 2
+const exercise2 = 4 * (2 + 6 / 3);
+console.log(exercise2)
+
+//Exercitiul 3
+const exercise3 = 2 * 3 * 3 + 2 * 2 * 2 * 2 * 2;
+console.log(exercise3);
+
+//Exercitiul 4
+const exercise4 = [5 * (3 + 2 / 2) + 3];
+console.log(exercise4);
+
+
+//Variables and operators - exercise
+
+//TODO 1
+var mathGrade = 10;
+var romanianGrade = 9.75;
+var biologyGrade = 8.8;
+
+var mediaAritmetica = (mathGrade + romanianGrade + biologyGrade) / 3;
+console.log(mediaAritmetica);
+
+//TODO 2
+var biologyGrade = 9.20;
+var mediaAritmetica = (mathGrade + romanianGrade + biologyGrade) / 3;
+console.log(mediaAritmetica);
+
+//TODO 3
+if (mathGrade = 10, mediaAritmetica > 9.50) {
+    console.log('Am luat notele pe care mi le-am dorit!')
+  }
